028f89bb2e47486a0eb0b2b1a99eff5930235dbc
Nikson <nikolaking88@gmail.com>
2024-06-29 22:09:07 +0200

    <feat> Created GUI for creating post

---
+++ AnimalHelp/Domain/Model/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHelp.Domain.Model
+{
+    public class Post
+    {
+        public PostState State { get; set; }
+        public DateTime PublishDate { get; set; }
+        public PostStatus Status { get; set; }
+        public string Description { get; set; }
+        public int Likes { get; set; }
+    }
+}
