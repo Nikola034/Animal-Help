2e748fb3880d634bb2c90b002bb8f07f96c87770
marijaparezanin <marija.parezanin@mensa.ba>
2024-06-30 14:30:20 +0200

    Fix constants bug

--- AnimalHelp/Repositories/Constants.cs
+++ AnimalHelp/Repositories/Constants.cs
@@ -4,15 +4,15 @@
     {
         public static readonly string DateFormat = "yyyy-MM-dd";
         
-        public const string VolunteerFilePath = "../../../Data/Volunteers.json";
-        public const string MemberFilePath = "../../../Data/Members.json";
-        public const string ProfileFilePath = "../../../Data/Profiles.json";
-        public const string PersonProfileMappingFilePath = "../../../Data/PersonProfile.json";
-        public const string AdminFilePath = "../../../Data/Admin.json";
+        public const string VolunteerFilePath = "../../Data/Volunteers.json";
+        public const string MemberFilePath = "../../Data/Members.json";
+        public const string ProfileFilePath = "../../Data/Profiles.json";
+        public const string PersonProfileMappingFilePath = "../../Data/PersonProfile.json";
+        public const string AdminFilePath = "../../Data/Admin.json";
 
 
-        public const string UserIdFilePath = "../../../Data/Id/UserId.txt";
-        public const string AdminIdFilePath = "../../../Data/Id/AdminId.txt";
+        public const string UserIdFilePath = "../../Data/Id/UserId.txt";
+        public const string AdminIdFilePath = "../../Data/Id/AdminId.txt";
 
     }
 }
