7003eaf052b4eb59a999296878f855cd1104df0d
Nikson <nikolaking88@gmail.com>
2024-06-30 13:25:01 +0200

    Solved conflicts

--- AnimalHelp/Domain/Model/Photo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AnimalHelp.Domain.Model
-{
-    public class Photo : IEntity
-    {
-        public string Id {  get; set; }
-        public string Url { get; set; }
-        public string Description { get; set; }
-    }
-}
