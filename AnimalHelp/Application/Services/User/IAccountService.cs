ce7de9b9736f91d79ced2f14435d551629087adf
Stefan Nikolic <stefan.nik04@gmail.com>
2024-07-01 18:26:48 +0200

    init agency info

--- AnimalHelp/Application/Services/User/IAccountService.cs
+++ AnimalHelp/Application/Services/User/IAccountService.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using AnimalHelp.Application.DTO;
+﻿using AnimalHelp.Application.DTO;
 using AnimalHelp.Domain.Model;
+using System;
 
 namespace AnimalHelp.Application.UseCases.User;
 public interface IAccountService
@@ -9,7 +8,7 @@ public interface IAccountService
     public void UpdateMember(string userId, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber);
     public void RegisterMember(RegisterMemberDto registerDto);
     public Volunteer RegisterVolunteer(RegisterVolunteerDto registerDto);
-    public Volunteer UpdateVolunteer(string volunteerId, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, DateTime dateJoined);
+    public Volunteer UpdateVolunteer(string volunteerId, string password, string name, string surname, DateTime birthDate, Gender gender, string phoneNumber, DateTime dateJoined, string email);
     public void DeleteMember(Member member);
     public string GetEmailByUserId(string userId, UserType userType);
     public bool IsEmailTaken(string email);
