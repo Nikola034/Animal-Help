0d24432722f29f92a35556e18450265f49ebed30
Nikola Jolovic <nikolajolovic02@gmail.com>
2024-07-01 19:57:03 +0200

    Update AnimalHelpViewModelFactory to use primary constructor

--- AnimalHelp/WPF/ViewModels/Factories/AnimalHelpViewModelFactory.cs
+++ AnimalHelp/WPF/ViewModels/Factories/AnimalHelpViewModelFactory.cs
@@ -1,60 +1,38 @@
-﻿using AnimalHelp.WPF.MVVM;
+﻿using System;
+using AnimalHelp.WPF.MVVM;
 using AnimalHelp.WPF.ViewModels.Admin;
 using AnimalHelp.WPF.ViewModels.Common;
 using AnimalHelp.WPF.ViewModels.Donations;
 using AnimalHelp.WPF.ViewModels.Member;
 using AnimalHelp.WPF.ViewModels.Volounteer;
-using System;
 
 namespace AnimalHelp.WPF.ViewModels.Factories;
 
-public class AnimalHelpViewModelFactory
+public class AnimalHelpViewModelFactory(
+    CreateViewModel<MainViewModel> createMainViewModel,
+    CreateViewModel<LoginViewModel> createLoginViewModel,
+    CreateViewModel<RegisterViewModel> createRegisterViewModel,
+    CreateViewModel<CreatePostViewModel> createPostViewModel,
+    CreateViewModel<AdminMenuViewModel> createAdminViewModel,
+    CreateViewModel<VolunteerRegistrationViewModel> createVolunteerRegistrationViewModel,
+    CreateViewModel<VolounteerMenuViewModel> createVolunteerMenuViewModel,
+    CreateViewModel<MemberMenuViewModel> createMemberMenuViewModel,
+    CreateViewModel<CreateDonationViewModel> createDonationViewModel)
     : IAnimalHelpViewModelFactory
 {
-    private readonly CreateViewModel<MainViewModel> _createMainViewModel;
-    private readonly CreateViewModel<LoginViewModel> _createLoginViewModel;
-    private readonly CreateViewModel<RegisterViewModel> _createRegisterViewModel;
-    private readonly CreateViewModel<CreatePostViewModel> _createPostViewModel;
-    private readonly CreateViewModel<AdminMenuViewModel> _createAdminViewModel;
-    private readonly CreateViewModel<VolunteerRegistrationViewModel> _createVolunteerRegistrationViewModel;
-    private readonly CreateViewModel<VolounteerMenuViewModel> _createVolunteerMenuViewModel;
-    private readonly CreateViewModel<MemberMenuViewModel> _createMemberMenuViewModel;
-    private readonly CreateViewModel<CreateDonationViewModel> _createDonationViewModel;
-
-    public AnimalHelpViewModelFactory(CreateViewModel<MainViewModel> createMainViewModel,
-        CreateViewModel<LoginViewModel> createLoginViewModel,
-        CreateViewModel<RegisterViewModel> createRegisterViewModel, CreateViewModel<CreatePostViewModel> createPostViewModel,
-        CreateViewModel<AdminMenuViewModel> createAdminViewModel,
-        CreateViewModel<VolunteerRegistrationViewModel> createVolunteerRegistrationViewModel,
-        CreateViewModel<VolounteerMenuViewModel> createVolunteerMenuViewModel,
-        CreateViewModel<MemberMenuViewModel> createMemberMenuViewModel,
-        CreateViewModel<CreateDonationViewModel> createDonationViewModel
-        )
-    {
-        _createMainViewModel = createMainViewModel;
-        _createLoginViewModel = createLoginViewModel;
-        _createRegisterViewModel = createRegisterViewModel;
-        _createPostViewModel = createPostViewModel;
-        _createAdminViewModel = createAdminViewModel;
-        _createVolunteerRegistrationViewModel = createVolunteerRegistrationViewModel;
-        _createVolunteerMenuViewModel = createVolunteerMenuViewModel;
-        _createMemberMenuViewModel = createMemberMenuViewModel;
-        _createDonationViewModel = createDonationViewModel;
-    }
-
     public ViewModelBase CreateViewModel(ViewType viewType)
     {
         return viewType switch
         {
-            ViewType.Main => _createMainViewModel(),
-            ViewType.Login => _createLoginViewModel(),
-            ViewType.Register => _createRegisterViewModel(),
-            ViewType.CreatePost => _createPostViewModel(),
-            ViewType.Admin => _createAdminViewModel(),
-            ViewType.VolunteerTable => _createVolunteerRegistrationViewModel(),
-            ViewType.VolounteerMenu => _createVolunteerMenuViewModel(),
-            ViewType.MemberMenu => _createMemberMenuViewModel(),
-            ViewType.CreateDonation => _createDonationViewModel(),
+            ViewType.Main => createMainViewModel(),
+            ViewType.Login => createLoginViewModel(),
+            ViewType.Register => createRegisterViewModel(),
+            ViewType.CreatePost => createPostViewModel(),
+            ViewType.Admin => createAdminViewModel(),
+            ViewType.VolunteerTable => createVolunteerRegistrationViewModel(),
+            ViewType.VolounteerMenu => createVolunteerMenuViewModel(),
+            ViewType.MemberMenu => createMemberMenuViewModel(),
+            ViewType.CreateDonation => createDonationViewModel(),
             _ => throw new ArgumentOutOfRangeException(nameof(viewType), viewType, "No ViewModel exists for the given ViewType: " + viewType)
         };
     }
