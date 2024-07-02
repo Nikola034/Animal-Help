e911a2cd840dacb57d7097fc26ced5404e189ef7
Nikola Jolovic <nikolajolovic02@gmail.com>
2024-07-02 21:46:33 +0200

    Add DonationDto

---
+++ AnimalHelp/Application/DTO/DonationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AnimalHelp.Domain.Model;
+using AnimalHelp.Domain.Model.Donations;
+
+namespace AnimalHelp.Application.DTO;
+
+public class DonationDto(Donation donation, Post? post)
+{
+    public string Id { get; set; } = donation.Id;
+    public DateTime DateTime { get; set; } = donation.DateTime;
+    public string Description { get; set; } = donation.Description;
+    public string From { get; set; } = donation.From;
+    public bool IsAnonymous { get; set; } = donation.IsAnonymous;
+    public Post? Post { get; set; } = post;
+}
